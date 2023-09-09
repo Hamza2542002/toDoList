cfe17e9ce769a73c64854ec0324645f6506e3b11
Hamza Mosaad <mezo2542002@gmail.com>
2023-09-09 15:50:42 +0200

    Modifying the Project And Solving some issues

--- JS/main.js
+++ JS/main.js
@@ -7,6 +7,8 @@ let comp1 = []
 let notStarted1 = []
 let inPro1 = []
 let bts = document.querySelectorAll(".parent >div button")
+
+
 if (localStorage.getItem('notStarted-tasks')){
   let parent = document.querySelector(".parent .not-started .tasks")
   let text = localStorage.getItem('notStarted-tasks');
@@ -34,6 +36,8 @@ if (localStorage.getItem('notStarted-tasks')){
   i++;
   }
 }
+
+
 
 if (localStorage.getItem('inPro-tasks')){
   let parent = document.querySelector(".parent .in-pro .tasks")
@@ -91,121 +95,170 @@ if (localStorage.getItem('comp-tasks')){
 }
 }
 
-bts.forEach(function(element){
-  element.onclick = function (){
-  i++;
-  if (i < 10){
-  index = "0"+i;
-  }else{
-  index = i
-  }
-  let parent = this.parentElement.firstElementChild;
-  console.log(parent)
-  let div = document.createElement("div");
-  div.draggable = true
-  div.id = i;
-  div.className = parent.id;
-  div.innerHTML = `<input type="text" value="Task ${index}"> <button class = "edit"><i class="fa-solid fa-pen-to-square"></i></button> <button class = "delete"><i class="fa-solid fa-trash"></i></button>`;
-  parent.appendChild(div);
-  let divDate = {
-  innerHTML:div.innerHTML,
-  id : div.id,
-  class : div.className
-  }
-  let json = JSON.stringify(divDate);
-  if(parent.id === 'not-s'){
-  notStarted1.push(divDate);
-  notStarted.push(json);
-  localStorage.setItem("notStarted-tasks",notStarted);
-  }else if (parent.id === 'in-p'){
-  inPro.push(json);
-  inPro1.push(divDate);
-  localStorage.setItem("inPro-tasks",inPro);
-  }else{
-  comp.push(json);
-  comp1.push(divDate);
-  localStorage.setItem("comp-tasks",comp);
-  }
-  console.log("parent ",JSON.stringify(parent.innerHTML))
-}
-})
 let editbts = document.querySelectorAll("button[class = 'edit']");
 let delbts = document.querySelectorAll("button[class = 'delete']");
 let inputs = document.querySelectorAll(".parent >div .tasks div input");
+delbts.forEach(function(element){
+  element.addEventListener("click",deleteFunc);
+})
 
-editbts.forEach((element)=>{
-  element.onclick = function(){
-  let input = element.parentElement.firstElementChild
-  // to set cursor at the end of the input field
-  const end = input.value.length;
-  input.setSelectionRange(end,end);
-  input.focus();
-  // -------------------------------------------
-  console.log(element.parentElement);
-  input.oninput = function(){
-    val = input.value;
-    console.log(val);
-    let inner = `<input type="text" value="${val}"> <button class = "edit"><i class="fa-solid fa-pen-to-square"></i></button> <button class = "delete"><i class="fa-solid fa-trash"></i></button>`;
-    // console.log(JSON.stringify(inner))
+editbts.forEach(function(element){
+  element.addEventListener("click",edit);
+})
+
+inputs.forEach(function(element){
+  element.addEventListener("input",editInputFunc);
+})
+
+bts.forEach(function(element){
+  element.onclick = function (){
+    i++;
+    i < 10 ? index = "0"+i : index =i;
+    let parent = this.parentElement.firstElementChild;
+    console.log(parent)
+    // div.draggable = true
+    // div.id = i;
+    // div.className = parent.id;
+    // div.appendChild(creatInput());
+    // div.appendChild(creatEditBt());
+    // div.appendChild(creatDelBt());
+    let div = creatTask(parent)
+    parent.appendChild(div);
     let divDate = {
-    innerHTML:inner,
-    id : element.parentElement.id,
-    class : element.parentElement.className
+      innerHTML:div.innerHTML,
+      id : div.id,
+      class : div.className
     }
     let json = JSON.stringify(divDate);
-    if (element.parentElement.className === 'not-s'){
-      for(let l = 0;l<notStarted.length;l++){
-        if (JSON.parse(notStarted[l]).id === divDate.id){
-          notStarted[l] = json;
-          break;
-        }
-      }
+    if(parent.id === 'not-s'){
+      notStarted1.push(divDate);
+      notStarted.push(json);
       localStorage.setItem("notStarted-tasks",notStarted);
-    }else if (element.parentElement.className === 'in-p'){
-      for(let l = 0;l<inPro.length;l++){
-        if (JSON.parse(inPro[l]).id === divDate.id){
-          console.log(JSON.parse(inPro[l]).id , divDate.id ,l)
-          console.log(json)
-          inPro[l] = json;
-          break;
-        }
-      }
+    }else if (parent.id === 'in-p'){
+      inPro.push(json);
+      inPro1.push(divDate);
       localStorage.setItem("inPro-tasks",inPro);
-    }
-    else{
-      for(let l = 0;l<comp.length;l++){
-        if (JSON.parse(comp[l]).id === divDate.id){
-          comp[l] = json;
-          break;
-        }
-      }
+    }else{
+      comp.push(json);
+      comp1.push(divDate);
       localStorage.setItem("comp-tasks",comp);
     }
-  }
+    console.log("parent ",JSON.stringify(parent.innerHTML))
   }
 })
 
-delbts.forEach((element)=>{
-  element.onclick = function(){
-  let temp = {
-    innerHTML:element.parentElement.innerHTML,
-    id : element.parentElement.id,
-    class : element.parentElement.className
+function creatTask(parent){
+  let div = document.createElement("div");
+  div.id = i;
+  div.className = parent.id;
+  div.draggable = true
+  div.addEventListener('dragstart',dragStart);
+  div.addEventListener('dragstart',dragEnd);
+  div.appendChild(creatInput());
+  div.appendChild(creatEditBt());
+  div.appendChild(creatDelBt());
+  return div
+}
+
+function creatInput(){
+  var element = document.createElement("input");
+  element.type = "text";
+  element.setAttribute("value","Task "+index);
+  element.addEventListener("input",editInputFunc);
+  return element;
+}
+
+function creatEditBt(){
+  var element = document.createElement("button");
+  element.className = "edit";
+  element.innerHTML = "<i class='fa-solid fa-pen-to-square'></i>";
+  element.addEventListener("click",edit);
+  return element;
+}
+
+function creatDelBt(){
+  var element = document.createElement("button");
+  element.className = "delete";
+  element.innerHTML = "<i class='fa-solid fa-trash'></i>";
+  element.addEventListener("click",deleteFunc);
+  return element;
+}
+
+function edit(){
+  console.log(this);
+  let input = this.parentElement.firstElementChild
+  setCursor(input);
+  console.log(this.parentElement);
+}
+
+
+function editInputFunc(){
+  val = this.value;
+  console.log(val);
+  this.setAttribute("value",val);
+  let divDate = {
+  innerHTML:this.parentElement.innerHTML,
+  id : this.parentElement.id,
+  class : this.parentElement.className
   }
-  if (element.parentElement.className === 'not-s'){
+  let json = JSON.stringify(divDate);
+  if (this.parentElement.className === 'not-s'){
+    for(let l = 0;l<notStarted.length;l++){
+      if (JSON.parse(notStarted[l]).id === divDate.id){
+        notStarted[l] = json;
+        break;
+      }
+    }
+    localStorage.setItem("notStarted-tasks",notStarted);
+  }else if (this.parentElement.className === 'in-p'){
+    for(let l = 0;l<inPro.length;l++){
+      if (JSON.parse(inPro[l]).id === divDate.id){
+        console.log(JSON.parse(inPro[l]).id , divDate.id ,l)
+        console.log(json)
+        inPro[l] = json;
+        break;
+      }
+    }
+    localStorage.setItem("inPro-tasks",inPro);
+  }
+  else{
+    for(let l = 0;l<comp.length;l++){
+      if (JSON.parse(comp[l]).id === divDate.id){
+        comp[l] = json;
+        break;
+      }
+    }
+    localStorage.setItem("comp-tasks",comp);
+  }
+}
+
+function setCursor(input){
+  const end = input.value.length;
+  input.setSelectionRange(end,end);
+  input.focus();
+}
+
+function deleteFunc(){
+  console.log(this)
+  let temp = {
+    innerHTML:this.parentElement.innerHTML,
+    id : this.parentElement.id,
+    class : this.parentElement.className
+  }
+  if (this.parentElement.className === 'not-s'){
     notStarted.splice(notStarted.indexOf(JSON.stringify(temp)), 1);
     localStorage.setItem("notStarted-tasks",notStarted);
-  }else if (element.parentElement.className === 'in-p'){
+  }else if (this.parentElement.className === 'in-p'){
     inPro.splice(inPro.indexOf(JSON.stringify(temp)), 1);
     localStorage.setItem("inPro-tasks",inPro);
   }else{
     comp.splice(comp.indexOf(JSON.stringify(temp)), 1);
     localStorage.setItem("comp-tasks",comp);
   }
-  element.parentElement.remove()
-  }
-})
-function del(div,parent){
+  this.parentElement.remove()
+}
+
+function del(div){
   let temp = {
     innerHTML:div.innerHTML,
     id : div.id,
@@ -254,33 +307,43 @@ function add(div,parent){
 
 let divs = document.querySelectorAll(".tasks div");
 let tasks = document.querySelectorAll(".tasks");
-let flag = false;
-console.log(divs)
-console.log(tasks)
-let drag = null;
-divs.forEach(element=>{
-  element.addEventListener('dragstart',function(){
-  console.log("drag start");
-  drag = element;
-  })
-  element.addEventListener('dragend',function(){
-  console.log("drag end");
-  drag = null;
-  })
+
+divs.forEach(function(element){
+  element.addEventListener('dragstart',dragStart);
+  element.addEventListener('dragstart',dragEnd);
 })
+
+let flag = false;
+let drag = null;
+
+tasks.forEach(function(element){
+  element.addEventListener('drop',drop);
+})
+
+function dragStart(){
+  console.log("drag start");
+  drag = this;
+}
+
+function dragEnd(){
+  console.log("drag End");
+  // drag = null; 
+}
+
 tasks.forEach((container)=>{
   container.addEventListener('dragover',function(e){
     e.preventDefault();
   });
   container.addEventListener('dragleave',function(e){
     e.preventDefault();
-  });
-    container.addEventListener('drop',function(){
-    if (container.id !== drag.className){
-      container.appendChild(drag);
-      console.log("drop");
-      add(drag,drag.parentElement)
-    }
-  });  
+  }); 
 })
-console.log(drag)
+
+function drop(){
+  console.log("drop");
+  if (this.id !== drag.className){
+    this.appendChild(drag);
+    add(drag,drag.parentElement)
+  }
+}
+
